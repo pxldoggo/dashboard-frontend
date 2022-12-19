07567d064b0115b6cdcf65ec438fe815ed6a4c7e
Vitagliano <gabrielrvita@gmail.com>
2022-12-19 12:26:35 -0300

    Coming soon page added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { fetchUser } from "../utils/api";
 import { DiscordUser } from "../utils/types";
+import Head from "next/head";
 
 type Props = {
   user: DiscordUser;
@@ -13,15 +14,65 @@ const Home: NextPage<Props> = ({ user }) => {
     window.location.href = `${process.env.API_URL}/auth/discord`;
   };
   return (
-    <div className="flex justify-center flex-col-reverse gap-8 items-center w-screen h-screen">
-      <button
-        onClick={handleLogin}
-        type="button"
-        className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-      >
-        Login with discord
-      </button>
-    </div>
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="@pxldoggo" />
+        <meta name="robots" content="index, follow" />
+        <meta name="language" content="English" />
+        <link rel="icon" type="image/svg+xml" href="favicon.ico" />
+
+        <title>Doggos</title>
+        <meta property="og:image" content="/banner.png" />
+        <meta name="theme-color" content="#569FF6" />
+        <meta name="title" content="Doggos" />
+        <meta name="og:title" content="Doggos" />
+        <meta
+          name="description"
+          content="Doggos is a web3 project that aims to provide the best tools for building social communities and projects, providing utility and ensuring security for other web3 projects."
+        />
+        <meta
+          property="og:description"
+          content="Doggos is a web3 project that aims to provide the best tools for building social communities and projects, providing utility and ensuring security for other web3 projects."
+        />
+        <meta
+          name="keywords"
+          content="Doggos, NFT, Doggo, Avalanche, Avalanche NFT, Animated NFT, Joepegs, Kalao, Avax, Generative NFT, Pixel Art, Doggos Labs"
+        />
+        <meta property="type" content="website" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_EN" />
+        <meta property="og:site_name" content="Doggos" />
+        <meta property="url" content="https://pixeldoggo.com/" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="text-xl font-pixellari relative bg-gradient-header flex h-screen w-full overflow-hidden">
+        <div className="w-full h-full flex items-center justify-center">
+          <div className="flex flex-col">
+            <div className="mb-12 ">
+              <img src="./logo.png" />
+            </div>
+            <div className="flex justify-center gap-4">
+              <a
+                target="_blank"
+                href="https://www.twitter.com/pxldoggo"
+                className="hover:scale-110 duration-100"
+              >
+                <img width={48} height={48} src="/twitter.png" />
+              </a>
+              <a
+                target="_blank"
+                href="https://discord.gg/rPA98J4xpQ"
+                className="hover:scale-110 duration-100"
+              >
+                <img width={48} height={48} src="/discord.png" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    // './public/**/*.html',
+    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+    "./context/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class", // 'media' is the default, change to 'class' if you want to use dark mode in with class names
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jakarta: ["Plus Jakarta Sans", "sans-serif"],
+        roboto: ["Roboto+Mono", "sans-serif"],
+        pixellari: ["Pixellari", "sans-serif"],
+      },
+      colors: {
+        "soft-blue-100": "#569ff6",
+        "soft-blue-200": "#4a8ee6",
+        "soft-blue-300": "#3d7dd6",
+        "soft-blue-400": "#316cc6",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("@headlessui/tailwindcss")({ prefix: "ui" })],
+};
