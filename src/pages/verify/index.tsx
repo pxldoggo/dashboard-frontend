c2400f1b253de51192ce12caa33987eed4ca5003
leafaar <yyyyrrzb@gmail.com>
2022-12-22 11:40:51 -0300

    Added: UserTwitterType, twt verify and twt image

--- src/pages/verify/index.tsx
+++ src/pages/verify/index.tsx
@@ -10,14 +10,17 @@ type Props = {
 };
 
 const VerifyPage: NextPage<Props> = ({ user }) => {
-  const handleLogin = () => {
+  const handleLoginDiscord = () => {
     window.location.href = `${process.env.API_URL}/auth/discord`;
+  };
+  const handleLoginTwitter = () => {
+    window.location.href = `${process.env.API_URL}/auth/twitter`;
   };
   return (
     <div className="flex justify-center flex-col-reverse gap-8 items-center w-screen h-screen">
       {!user.discord ? (
         <button
-          onClick={handleLogin}
+          onClick={handleLoginDiscord}
           type="button"
           className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
         >
@@ -33,6 +36,26 @@ const VerifyPage: NextPage<Props> = ({ user }) => {
             height={50}
           />
           Logged as {user.discord.user?.username}
+        </div>
+      )}
+      {!user.twitter ? (
+        <button
+          onClick={handleLoginTwitter}
+          type="button"
+          className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          Login with Twitter
+        </button>
+      ) : (
+        <div className="items-center flex flex-row gap-3">
+          <Image
+            className="rounded-full"
+            alt="Twitter user profile image"
+            src={`${user.twitter.user?.profile_image_url}`}
+            width={50}
+            height={50}
+          />
+          Logged as {user.twitter.user?.name}
         </div>
       )}
       <ConnectButton chainStatus={"none"} showBalance={false} />
