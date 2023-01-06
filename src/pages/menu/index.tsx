f9a21b921dacd99f078c6fa70a643f028afab708
leafaar <yyyyrrzb@gmail.com>
2023-01-06 17:59:00 -0300

    fixing some errors

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,4 +1,5 @@
 import { GetServerSidePropsContext, NextPage } from "next";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { GuildMenuItem } from "../../components/guilds/GuildMenuItem";
 import { fetchMutualGuilds } from "../../utils/api";
@@ -11,11 +12,21 @@ type Props = {
 
 const MenuPage: NextPage<Props> = ({ guilds, adminGuilds }) => {
   const router = useRouter();
-  console.log(guilds);
+  const getBotAuthLink = (guildId: string) => {
+    return `https://discord.com/oauth2/authorize?client_id=1050777933754663012&scope=bot&permissions=2147483656&guild_id=${guildId}`;
+  };
+  const filteredGuilds: DiscordGuild[] = adminGuilds.filter((guild) =>
+    guilds.some((withBotGuild) => withBotGuild.id != guild.id)
+  );
   return (
     <div className="p-12 bg-[#1d1d1f] h-screen">
-      <div className={"w-1/4 grid grid-flow-row"}>
-        <h1 className={"font-normal"}>Please Select a Discord Server</h1>
+      <h1 className={"font-normal text-4xl pb-16"}>
+        Please Select a Discord Server
+      </h1>
+      <h1 className={"font-normal pb-2"}>
+        Servers that you are admin and the bot is in it.
+      </h1>
+      <div className="grid w-full grid-cols-3 gap-3 border border-red-500 rounded-xl p-12">
         {guilds.map((guild) => (
           <div
             key={guild.id}
@@ -24,12 +35,18 @@ const MenuPage: NextPage<Props> = ({ guilds, adminGuilds }) => {
             <GuildMenuItem color="1d1d1f" guild={guild} />
           </div>
         ))}
-        {adminGuilds.map((guild) => (
-          <div
-            key={guild.id}
-            onClick={() => router.push(`/dashboard/${guild.id}`)}
-          >
-            <GuildMenuItem color="fbfbfd" guild={guild} />
+      </div>
+      <h1 className={"font-normal pb-2 pt-12"}>Servers that you are admin.</h1>
+      <div className="grid w-full grid-cols-3 gap-3 border border-red-500 rounded-xl p-12">
+        {filteredGuilds.map((guild) => (
+          <div key={guild.id}>
+            <Link
+              href={getBotAuthLink(guild.id)}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <GuildMenuItem color="fbfbfd" guild={guild} />
+            </Link>
           </div>
         ))}
       </div>
